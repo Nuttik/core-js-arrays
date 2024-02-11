6cd5b4c0ea3cc538916c5f83d24d7514f1e58ded
Natalia Repkina <pn.88@mail.ru>
2024-02-11 15:34:54 +0300

    feat: add solution generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -376,8 +376,15 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) return [];
+  let num = 1;
+  const array = new Array(len).fill(0);
+  return array.map(() => {
+    const value = num;
+    num += 2;
+    return value;
+  });
 }
 
 /**
